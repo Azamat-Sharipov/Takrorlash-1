2b957e64a31012244ed525f570a720097c900266
Azamat-Sharipov <navom65@gmail.com>
2023-12-01 10:40:05 -0800

    first

---
+++ main.py
@@ -0,0 +1,11 @@
+number = int(input("Son: "))
+if number % 2 == 0:
+    print("Rahmat!")
+
+else:
+    print("Bu juft son emas: ")
+
+    while True:
+        n = int(input("olma-"))
+        m = int(input("o`quvchilar-"))
+        print(n % m)
